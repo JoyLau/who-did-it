7f6587d585e1b5619ba0de9ff846a2f0be0bd1a5
JoyLau <2587038142@qq.com>
2018-10-18 08:56:55 +0800

    use GRAYED_SMALL_ITALIC_ATTR

--- src/main/java/cn/joylau/code/utils/VCSUtils.java
+++ src/main/java/cn/joylau/code/utils/VCSUtils.java
@@ -8,6 +8,7 @@ import com.intellij.openapi.vcs.diff.DiffMixin;
 import com.intellij.openapi.vcs.history.VcsRevisionDescription;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.ui.SimpleTextAttributes;
+import com.intellij.util.ui.UIUtil;
 import com.intellij.vcsUtil.VcsUtil;
 import org.apache.commons.lang3.time.DateUtils;
 
@@ -21,7 +22,11 @@ import java.util.Date;
  */
 public class VCSUtils {
 
-    public static PresentableNodeDescriptor.ColoredFragment getNodeDesc(Project project, VirtualFile virtualFile){
+    private static final int SMALL_ITALIC_STYLE = SimpleTextAttributes.STYLE_SMALLER + SimpleTextAttributes.STYLE_ITALIC;
+
+    private static final SimpleTextAttributes GRAYED_SMALL_ITALIC_ATTR = new SimpleTextAttributes(SMALL_ITALIC_STYLE, UIUtil.getInactiveTextColor());
+
+    public static PresentableNodeDescriptor.ColoredFragment getNodeDesc(Project project, VirtualFile virtualFile) {
         AbstractVcs vcs = ProjectLevelVcsManager.getInstance(project).getVcsFor(virtualFile);
         if (vcs == null) return null;
 
@@ -32,15 +37,15 @@ public class VCSUtils {
 
                 String author = description.getAuthor();
                 String date = getDate(description.getRevisionDate());
-                return new PresentableNodeDescriptor.ColoredFragment("  " + date + "  " + author,
+                return new PresentableNodeDescriptor.ColoredFragment("   " + date + "  " + author,
                         description.getCommitMessage(),
-                        SimpleTextAttributes.GRAYED_SMALL_ATTRIBUTES);
+                        GRAYED_SMALL_ITALIC_ATTR);
             }
         }
         return null;
     }
 
-    private static String getDate(Date date){
-        return DateUtils.isSameDay(new Date(),date) ? "Today" : new SimpleDateFormat("yyyy/MM/dd").format(date);
+    private static String getDate(Date date) {
+        return DateUtils.isSameDay(new Date(), date) ? "Today" : new SimpleDateFormat("yyyy/MM/dd").format(date);
     }
 }
