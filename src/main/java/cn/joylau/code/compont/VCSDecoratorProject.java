85b83a3c28cca1cb8a8c8c38c3b11f904b223a6c
JoyLau <2587038142@qq.com>
2018-10-19 16:44:13 +0800

    upload module

--- src/main/java/cn/joylau/code/compont/VCSDecoratorProject.java
+++ src/main/java/cn/joylau/code/compont/VCSDecoratorProject.java
@@ -6,11 +6,15 @@ import com.intellij.ide.projectView.ProjectView;
 import com.intellij.ide.util.treeView.PresentableNodeDescriptor;
 import com.intellij.openapi.components.AbstractProjectComponent;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.vcs.changes.ChangeList;
+import com.intellij.openapi.vcs.changes.ChangeListListener;
+import com.intellij.openapi.vcs.changes.ChangeListManager;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.openapi.vfs.VirtualFileEvent;
 import com.intellij.openapi.vfs.VirtualFileListener;
 import com.intellij.openapi.vfs.VirtualFilePropertyEvent;
 import com.intellij.openapi.vfs.ex.VirtualFileManagerEx;
+import com.intellij.util.messages.MessageBusConnection;
 import org.jetbrains.annotations.NotNull;
 
 /**
@@ -22,12 +26,18 @@ public class VCSDecoratorProject extends AbstractProjectComponent {
 
     private final Project project;
 
+    private final MessageBusConnection messageBus;
+
     private final VirtualFileListener vfListener;
+
+    private final ChangeListListener changeListListener;
 
     protected VCSDecoratorProject(Project project) {
         super(project);
 
         this.project = project;
+
+        this.messageBus = this.project.getMessageBus().connect();
 
         this.vfListener = new VirtualFileListener() {
             @Override
@@ -38,10 +48,16 @@ public class VCSDecoratorProject extends AbstractProjectComponent {
             public void contentsChanged(@NotNull VirtualFileEvent event) {
                 refresh(event.getFile());
             }
-
             @Override
             public void fileDeleted(@NotNull VirtualFileEvent event) {
                 delete(event.getFile());
+            }
+        };
+
+        this.changeListListener = new ChangeListListener() {
+            @Override
+            public void changeListChanged(ChangeList fromList) {
+                System.out.println(fromList);
             }
         };
     }
@@ -51,10 +67,9 @@ public class VCSDecoratorProject extends AbstractProjectComponent {
             PresentableNodeDescriptor.ColoredFragment coloredFragment = VCSUtils.getNodeDesc(project,virtualFile);
             if (coloredFragment != null) {
                 VCSDecoratorCache.getInstance().getMap().put(virtualFile.getPath(),coloredFragment);
+                ProjectView.getInstance(this.project).refresh();
             }
         }
-
-        ProjectView.getInstance(this.project).refresh();
     }
 
     private void delete(VirtualFile virtualFile){
@@ -64,10 +79,14 @@ public class VCSDecoratorProject extends AbstractProjectComponent {
     @Override
     public void initComponent() {
         VirtualFileManagerEx.getInstance().addVirtualFileListener(this.vfListener);
+        ChangeListManager.getInstance(project).addChangeListListener(this.changeListListener);
     }
 
     @Override
     public void disposeComponent() {
+        if (this.messageBus != null) {
+            this.messageBus.disconnect();
+        }
         if (this.vfListener != null) {
             VirtualFileManagerEx.getInstance().removeVirtualFileListener(this.vfListener);
         }
