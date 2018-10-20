8d9980b3203a3ff2edf15a31e6b7dba990a4571e
JoyLau <2587038142@qq.com>
2018-10-20 11:46:46 +0800

    upload module

---
+++ src/main/java/cn/joylau/code/action/EnableAction.java
@@ -0,0 +1,32 @@
+package cn.joylau.code.action;
+
+import cn.joylau.code.settings.WhoDidSettings;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.ToggleAction;
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * Created by joylau on 2018/10/19.
+ * cn.joylau.code.action
+ * 2587038142@qq.com
+ */
+public class EnableAction extends ToggleAction {
+    public EnableAction() {
+        super();
+    }
+
+    @Override
+    public boolean isSelected(AnActionEvent e) {
+        return e.getProject() != null && WhoDidSettings.getInstance().isInfoEnable();
+    }
+
+    @Override
+    public void setSelected(AnActionEvent e, boolean state) {
+    }
+
+    @Override
+    public void update(@NotNull AnActionEvent e) {
+        e.getPresentation().setEnabled(e.getProject()!=null);
+        super.update(e);
+    }
+}
